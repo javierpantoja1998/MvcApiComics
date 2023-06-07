909b190fa9962362b677a4837eeb185f704e2ba9
javierpantoja1998 <cientificodebanco@gmail.com>
2023-06-07 12:39:57 +0200

    Add project files.

---
+++ MvcApiComics/Controllers/ComicsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiComics.Models;
+using MvcApiComics.Services;
+
+namespace MvcApiComics.Controllers
+{
+    public class ComicsController : Controller
+    {
+        private ServiceApiComics service;
+
+        public ComicsController(ServiceApiComics service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Comic> comics = await this.service.GetComicsAsync();
+            return View(comics);
+        }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Comic comic = await this.service.FindComicAsync(id);
+            return View(comic);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Comic comic)
+        {
+            await this.service.CreateComic(comic);
+            return RedirectToAction("Index");
+        }
+
+
+
+        
+
+    }
+}
---
+++ MvcApiComics/Models/Comic.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcApiComics.Models
+{
+    
+    public class Comic
+    {
+        public int IdComic { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen{ get; set; }
+    }
+}
